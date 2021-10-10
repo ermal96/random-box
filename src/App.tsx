62bcefb3605819953d51f105aef720b4d62f3700
Ermal Vrapi <ermalvrapi18@gmail.com>
2021-10-10 22:24:29 +0200

    feat: add baseweb ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Button, SIZE, SHAPE } from "baseui/button";
 
 const App: React.FC = () => {
   return (
     <>
-      <button>Button</button>
+      <Button
+        size={SIZE.compact}
+        shape={SHAPE.pill}
+      >
+        Hello
+      </Button>
     </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { Provider as StyletronProvider } from 'styletron-react';
+import { LightTheme, BaseProvider } from 'baseui';
+
+const engine = new Styletron();
+
 
 ReactDOM.render(
-  <App />,
+  <StyletronProvider value={engine}>
+    <BaseProvider theme={LightTheme}>
+      <App />
+    </BaseProvider>
+  </StyletronProvider>,
   document.getElementById('root')
 );
 
